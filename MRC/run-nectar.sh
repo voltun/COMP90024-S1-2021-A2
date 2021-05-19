1139d8681e865c32a4f2ed8b56b2a51627da08e8
voltun <nicholaswongcl@gmail.com>
2021-05-19 18:38:59 +0930

    WIP Ansible Debug

--- MRC/run-nectar.sh
+++ MRC/run-nectar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# NOTE: Requires access email as argument i.e. ./run-nectar.sh <email>
+# NOTE: Requires access email as argument 1 
+# i.e. ./run-nectar.sh <email>
 
-. ./MRC_COMP90024_API.sh $1; ansible-playbook mrc.yaml
+. ./MRC_COMP90024_API.sh $1; ansible-playbook --ask-become-pass -vvv mrc.yaml
