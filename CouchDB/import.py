d33be6561b8bc4202ed524c011057b2887ae1c1d
Yajing <d791524213@gmail.com>
2021-05-21 23:43:22 +0800

    CouchDB

---
+++ CouchDB/import.py
@@ -0,0 +1,24 @@
+import json
+import couchdb
+
+# Connect to CouchDB
+user = "admin"
+password = "1234"
+couchserver = couchdb.Server("http://%s:%s@172.26.129.212:5984/" % (user, password))
+dbname = "twitter"
+
+# Create a new database if not existed
+if dbname in couchserver:
+    db = couchserver[dbname]
+else:
+    db = couchserver.create(dbname)
+
+# Open file
+file1 = open('TwitterData.txt', 'r')
+
+# Import each line of file to couchdb/twitter
+for line in file1:
+    doc = json.loads(line)
+    docid = str(doc['id'])
+    db[docid] = docc
+
