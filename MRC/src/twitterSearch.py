88195b702f21367026d1109acbd9a70e979e2e5b
lcho0006 <54564885+lcho0006@users.noreply.github.com>
2021-05-25 04:07:05 +1000

    Update twitterSearch.py

--- MRC/src/twitterSearch.py
+++ MRC/src/twitterSearch.py
@@ -1,44 +1,27 @@
-import tweepy, json, configparser, os
-from getPostCode import getPostCode
+import tweepy, json
 
-# Obtain full file path
-pardir = os.path.dirname(os.path.abspath(__file__))
-filepath = os.path.join(pardir, 'auth.cfg')
+def twitterSearch(config):
 
-# Setup configparser to read in authentication credentials
-config = configparser.ConfigParser()
-config.read(filepath)
+    access_token = config.get('Twitter_Auth','access_token')
+    access_token_secret = config.get('Twitter_Auth','access_secret')
+    consumer_key = config.get('Twitter_Auth','api_key')
+    consumer_secret = config.get('Twitter_Auth','api_secret_key')
 
-access_token = config.get('Twitter_Auth','access_token')
-access_token_secret = config.get('Twitter_Auth','access_secret')
-consumer_key = config.get('Twitter_Auth','api_key')
-consumer_secret = config.get('Twitter_Auth','api_secret_key')
+    # Establish authentication to Twitter's API
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_token_secret)
+    api = tweepy.API(auth)
 
-# Establish authentication to Twitter's API
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
+    #set Australia as a filter
+    places = api.geo_search(query="Australia",granularity="country")
+    place_id = places[0].id
 
-api = tweepy.API(auth)
-places = api.geo_search(query="Australia",granularity="country")
-place_id = places[0].id
-api.wait_on_rate_limit = True
-tweets = tweepy.Cursor(api.search,
-                       q="place:%s" % place_id,
-                       lang="en").items(1000000)
+    #set wait time to prevent error
+    api.wait_on_rate_limit = True
+    tweets = tweepy.Cursor(api.search,q="place:%s" % place_id,lang="en").items(1000000)
 
-file=open("tweetSearch.txt","w")
-
-count = 0
-for tweet in tweets:
-    count+=1
-    if tweet.coordinates != None:
-        tweet = tweet._json
-        long,lat = tweet['coordinates']['coordinates']
-        postcode = getPostCode(lat,long)
-        if postcode == None:
-            pass
-        else:
-            temp = {"postcode":postcode}
-            tweet['coordinates'].update(temp)
-            file.write(json.dumps(tweet)+ '\n')
-
+    #output data onto file
+    file=open("tweetSearch.txt","w")
+    for tweet in tweets:
+        if tweet.coordinates != None:
+            file.write(json.dumps(tweet)+'\n')
